99c82a221216f39c37343bc1456d4f731980c263
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2026-09-19 18:18:40 +0530

    update

--- Python/input_fun.py
+++ Python/input_fun.py
@@ -1,31 +1,31 @@
-# a=input("enter your name :")
-# print(a)
+a=input("enter your name :")
+print(a)
 
-# name=input("enter yor name :")
-# print("Welcome",name)
+name=input("enter yor name :")
+print("Welcome",name)
 
-# num1=input("enter a number1 :")
-# num2=input("enter a number2: ")
-# sum=num1+num2 # By difault Consider String
-# print(sum)
-
-
-# a=int(input("enter a number:"))
-# b=int(input("Enter 2nd num:"))
-# print(a+b)
+num1=input("enter a number1 :")
+num2=input("enter a number2: ")
+sum=num1+num2 # By difault Consider String
+print(sum)
 
 
-# a=float(input("enter 1st num:"))
-# b=float(input("enter 2nd num:"))
-# print(a*b)
+a=int(input("enter a number:"))
+b=int(input("Enter 2nd num:"))
+print(a+b)
 
 
-# calculate avg of 2 number
-# a=float(input("enter 1st number:"))
-# b=float(input("enter 2nd number:"))
-# sum=a+b
-# avg=sum/2
-# print(avg)
+a=float(input("enter 1st num:"))
+b=float(input("enter 2nd num:"))
+print(a*b)
+
+
+#calculate avg of 2 number
+a=float(input("enter 1st number:"))
+b=float(input("enter 2nd number:"))
+sum=a+b
+avg=sum/2
+print(avg)
 
 a=float(input("enter 1st number:"))
 b=float(input("enter 2nd number:"))
