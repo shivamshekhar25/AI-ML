e3b8cedce31991364e7a3339dcfcefa085300def
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2026-09-19 19:34:41 +0530

    Problems

---
+++ Python/Assignment_1.py
@@ -0,0 +1,107 @@
+#Q1
+
+name=input("Enter your name:")
+Age=int(input("Enter Your Age:"))
+print("Hello",name ,",you Are", Age ,"Years Old!")
+
+
+#Q2
+
+a=float(input("Enter Num1:"))
+b=float(input("Enter Num2:"))
+sum=a+b
+diff=a-b
+product=a*b 
+Quat=a/b
+print(sum)
+print(diff)
+print(Quat)
+print(product)
+
+#Q3
+
+a=float(input("Enter a integer:"))
+b=float(input("enter a integer:"))
+c=float(input("enter a float:"))
+
+avg=(a+b+c)/3
+print(avg)
+
+
+#Q4
+
+num=(input("Enter a number:"))
+integer=int(num)
+floate=float(num)
+string=str(num)
+print(integer,type(integer))
+print(floate,type(floate))
+print(string,type(string))
+
+#Q5
+
+x=10+3*2**2
+print(x)
+
+
+#Q6
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c=a
+a=b
+b=c
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+
+                      #or
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+a,b=b,a
+print("After swapping:")
+print( a)
+print( b)
+
+#Q7
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * (9 / 5)) + 32
+
+print("Temperature in Fahrenheit =", fahrenheit)
+
+
+#Q8
+
+r=float(input("Enter Radius:"))
+area=3.14*r**2
+print(area)
+
+
+#Q9
+
+p=float(input("Enter Principal:"))
+r=float(input("Enter Rate:"))
+t=float(input("enter time"))
+
+SI=(p*r*t)/100
+
+print("Simple Intrest=" ,SI)
+
+
+#Q10
+
+num=float(input("Enter a number:"))
+
+integer=int(num)
+fractional= num-integer
+
+print(integer)
+print(fractional)
+
+
+#######      complete Assignment
+
+
