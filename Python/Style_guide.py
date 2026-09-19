a572222695d07bf268e43e2391250c837baaf799
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2026-09-19 14:30:49 +0530

    New Practice

---
+++ Python/Style_guide.py
@@ -0,0 +1,16 @@
+#snake_case use in python
+
+my_name="shivam"
+my_phone_no="889798...."
+your_age="18"
+
+#sum of two numbers
+a=10
+b=15
+print(a+b)
+
+
+a=70
+b=53
+sum=a+b
+print(sum)
---
+++ Python/comments.py
@@ -0,0 +1,4 @@
+# My name Is shivam 
+# Python Topic
+# 1.data type
+# 2.variable
