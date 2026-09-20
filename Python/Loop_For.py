11609701e27d5ef1389db0967a411a2ce66ce67d
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2026-09-20 21:50:26 +0530

    update

---
+++ Python/Loop_For.py
@@ -0,0 +1,33 @@
+string="shivam"
+for var in string:
+    print(var)
+
+
+
+for i in range(5):
+    print(i)
+
+
+
+for i in range(5):
+    print(i+1)
+
+
+word="artificial"
+ans=0
+for char in word:
+    if char=="i":
+        ans+=1
+print(ans)
+
+
+
+
+
+
+word="artificial"
+ans=0
+for char in word:
+    if char=="a" or char=="e" or char=="i" or char=="o"  or char=="u":
+        ans+=1
+print(ans)
