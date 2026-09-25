075325b86f0ee3f7a142d5661c9c1dd7a8a97199
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2026-09-25 15:06:46 +0530

    Complete Assignment

--- Python/Assignment_2.py
+++ Python/Assignment_2.py
@@ -45,3 +45,121 @@ def count_digits(n):
 
 n = int(input("Enter a number: "))
 print("Number of digits:", count_digits(n))
+
+
+
+# Q5
+
+def sum_digits(n):
+    total = 0
+
+    while n > 0:
+        digit = n % 10
+        total = total + digit
+        n = n // 10
+
+    return total
+
+
+n = int(input("Enter a number: "))
+print("Sum of digits:", sum_digits(n))
+
+
+
+# Q6
+
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print(i)
+
+
+
+# Q7
+
+
+while True:
+    n = input("Enter a number or Quit: ")
+
+    if n == "Quit":
+        break
+    n=int(n)
+
+    if n > 0:
+        print("Positive")
+    elif n < 0:
+        print("Negative")
+    else:
+        print("Zero")
+
+
+
+ # Q8
+
+def calculator(a, b, operation):
+
+    if operation == "+":
+        return a + b
+
+    elif operation == "-":
+        return a - b
+
+    elif operation == "*":
+        return a * b
+
+    elif operation == "/":
+        return a / b
+
+    else:
+        return "Invalid operation"
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+print("Answer:", calculator(a, b, operation))
+
+# Q9
+
+
+def is_prime(n):
+
+    if n < 2:
+        return False
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+n = int(input("Enter a number: "))
+
+if is_prime(n):
+    print("Prime number")
+else:
+    print("Not a prime number")
+
+
+# Q10
+
+
+secret_number = 50
+
+while True:
+
+    guess = int(input("Guess the number: "))
+
+    if guess > secret_number:
+        print("Too high")
+
+    elif guess < secret_number:
+        print("Too low")
+
+    else:
+        print("Correct!")
+        break
+
+    ##Complete Assignment
